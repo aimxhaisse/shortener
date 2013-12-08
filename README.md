4f8324219e5a1a8bba90be3009d7c1ea88baff9f
s. rannou <mxs@sbrk.org>
2013-12-08 15:21:54 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# shortener
+
+Yes another URL shortener
+
+## License
+
+Copyright © 2013 s. rannou <mxs@sbrk.org>
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject shortener "0.1.0-SNAPSHOT"
+  :description "Yet Another URL Shortener"
+  :url "http://302.sbrk.org/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.2.1"]
+                 [ring/ring-devel "1.2.1"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :dev-dependencies [[lein-run "0.8.8"]])
